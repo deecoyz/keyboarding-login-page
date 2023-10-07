6125d38cfb3131f44ad288606f7832cb8a2b6139
canvas <123119255+deecoyz@users.noreply.github.com>
2023-10-06 22:44:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
   } 
   if (username === "keyboarder-q5x8s" && password === "4zw68") {
     window.location.href = "https://deecoyz.github.io/keyboarding-settings/";
+  } 
+  if (username === "keyboarder-y04eg" && password === "o4o1u") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-stats/";
+  }
+  if (username === "keyboarder-vb1ot" && password === "7xchz") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-summary/";
+  } 
+  if (username === "keyboarder-6sp0d" && password === "i21hp") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-info/";
   } else {
-    document.getElementById("message").textContent = "Invalid credentials. Sign up for Keyboarding Practice now for only $4.99 a month.";
+    document.getElementById("message").textContent = "Invalid credentials. Sign up for Keyboarding Practice now for only $9.99 a year!";
   }
 });
