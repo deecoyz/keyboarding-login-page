3293df08b918eb0904c6d91460de881a00e782bf
canvas <123119255+deecoyz@users.noreply.github.com>
2023-10-07 14:33:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,9 +27,7 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
   if (username === "keyboarder-ka29m" && password === "20dka") {
     window.location.href = "https://deecoyz.github.io/keyboarding-class-game1";
   }
-  if (username === "maxm2012" && password === "brandonms") {
-    window.location.href = "https://monkeytype.com";
-  } else {
+  else {
     document.getElementById("message").textContent = "Invalid credentials. Sign up for Keyboarding Practice now for only $9.99 a year!";
   }
 });
