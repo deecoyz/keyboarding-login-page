e78e5b05518e1d119550d888188ab8532e41a010
canvas <123119255+deecoyz@users.noreply.github.com>
2023-10-07 17:12:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
   if (username === "keyboarder-ka29m" && password === "20dka") {
     window.location.href = "https://deecoyz.github.io/keyboarding-class-game1";
   }
+  if (username === "keyboarder-ijdoa" && password === "d34hr") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-class-homerow";
+  }
+  if (username === "keyboarder-c230i" && password === "j0ci1") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-practice-pace";
+  }
+  if (username === "keyboarder-eruik" && password === "f2039") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-class-test";
+  }
+  if (username === "keyboarder-2d93j" && password === "a0c8h") {
+    window.location.href = "https://deecoyz.github.io/keyboarding-class-test2";
+  }
   else {
     document.getElementById("message").textContent = "Invalid credentials. Sign up for Keyboarding Practice now for only $9.99 a year!";
   }
